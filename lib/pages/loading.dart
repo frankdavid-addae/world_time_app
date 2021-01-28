310f16208ee899f60186cda4467d6a64e4d4ac61
Frank David Addae <frankdavid.addae@gmail.com>
2021-01-28 09:18:28 +0000

    first commit

---
+++ lib/pages/loading.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:world_time_app/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  void setupWorldTime() async {
+    WorldTime worldTime =
+        WorldTime(location: 'Accra', flag: 'ghana.png', url: 'africa/accra');
+    await worldTime.getTime();
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': worldTime.location,
+      'flag': worldTime.flag,
+      'time': worldTime.time,
+      'isDaytime': worldTime.isDaytime,
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[900],
+      body: Center(
+        child: Container(
+          constraints: BoxConstraints.expand(),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/clock2.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: SpinKitRipple(
+            color: Colors.white,
+            size: 100.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
